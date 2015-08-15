cabb78903c8470b4f3ff97dd618bab9c3ea70560
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2015-08-15 15:14:04 -0400

    Implementation of fold_lower_values

--- lib/player.ex
+++ lib/player.ex
@@ -9,6 +9,18 @@ defmodule Player do
     game = if Enum.count(possible_plays) == 0 do
       Game.finish(game)
     else
+      sort_fn = if :random.uniform < player.fold_lower_values do
+        fn {a1, a2}, {b1, b2} ->
+          (a1 + a2) <= (b1 + b2)
+        end
+      else
+        fn {a1, a2}, {b1, b2} ->
+          (a1 + a2) >= (b1 + b2)
+        end
+      end
+
+      possible_plays = Enum.sort(possible_plays, sort_fn)
+      
       play = List.first(possible_plays)
       
       Game.fold_tiles(game, play)

ee5c74818fd3ebfcff68a44c962a671f921caf42
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2015-08-16 12:07:07 -0400

    test case for already folded tiles

--- test/game_test.exs
+++ test/game_test.exs
@@ -1,6 +1,6 @@
 defmodule GameTest do
   use ExUnit.Case
-  
+
   test "decompose pairs should add up to n" do
     for n <- 1..12 do
       for {a, b} <- Game.decompose(n) do
@@ -18,7 +18,7 @@ defmodule GameTest do
 
   test "can_fold? all tiles in a new game" do
     game = %Game{}
-    
+
     for i <- 1..12 do
       for j <- 1..12 do
         assert Game.can_fold?(game, {i, j})
@@ -26,13 +26,26 @@ defmodule GameTest do
     end
   end
 
-  test "fold_tiles should be removed for the tile set" do
+  test "fold_tiles tiles should be removed from the tile set" do
     for i <- 1..12 do
       for j <- 1..12 do
         game = Game.fold_tiles(%Game{}, {i, j})
 
         assert !Set.member?(game.tiles, i)
         assert !Set.member?(game.tiles, j)
+      end
+    end
+  end
+
+  test "fold_tiles tiles are folded only if they can both be folded" do
+    for i <- 1..12 do
+      for j <- 1..12 do
+        game = Game.fold_tiles(%Game{}, {i, j})
+
+        for k <- 1..12 do
+          assert Game.fold_tiles(game, {i, k}) == game
+          assert Game.fold_tiles(game, {j, k}) == game
+        end
       end
     end
   end

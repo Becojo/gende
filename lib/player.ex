9d3627898fdac052887fafb8830746827b2d2a79
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2015-08-16 11:58:31 -0400

    best strategy so far

--- lib/player.ex
+++ lib/player.ex
@@ -4,26 +4,12 @@ defmodule Player do
 
   def play(%{state: :ok} = game, player) do
     dices = Game.rand
-    possible_plays = Game.possible_plays(game, dices)
+    possible_plays = Enum.sort(Game.possible_plays(game, dices))
 
     game = if Enum.count(possible_plays) == 0 do
       Game.finish(game)
     else
-      sort_fn = if :random.uniform < player.fold_lower_values do
-        fn {a1, a2}, {b1, b2} ->
-          (a1 + a2) <= (b1 + b2)
-        end
-      else
-        fn {a1, a2}, {b1, b2} ->
-          (a1 + a2) >= (b1 + b2)
-        end
-      end
-
-      possible_plays = Enum.sort(possible_plays, sort_fn)
-      
-      play = List.first(possible_plays)
-      
-      Game.fold_tiles(game, play)
+      Game.fold_tiles(game, List.first(possible_plays)) 
     end
     
     play(game, player)
